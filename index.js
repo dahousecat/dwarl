80fa83bea580162755cc570dd903c63f4c582c88
felix <felix@felixeve.co.uk>
2024-08-19 23:48:06 +0100

    dwarl npm package

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-export class dwarl {
-  static test() {
-    return "dwarl test"
+import axios from 'axios'
+import NoteyApiError from '../../src/lib/error/NoteyApiError'
+
+export default class dwarl {
+  baseUrl = ''
+
+  constructor({ baseUrl }) {
+    this.baseUrl = baseUrl
   }
+
+  async registerEmail(email) {
+    const url = this.baseUrl + '/dwarl/register/email'
+
+    const options = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    }
+
+    const data = new URLSearchParams()
+    data.set('email', email)
+
+    let response
+    try {
+      response = await axios.post(url, data, options)
+    } catch (error) {
+      console.error(error)
+    }
+
+    console.log(response, 'response')
+
+    return response.data;
+  }
+
 }
