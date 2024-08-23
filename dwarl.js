3b0cbd54c526f3e2a77e1ede53ce1b33478b5149
Felix <dahousecat@gmail.com>
2024-08-23 20:30:40 +0100

    remove line

--- dwarl.js
+++ dwarl.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import { startAuthentication, startRegistration } from "@simplewebauthn/browser";
-import { startRegistration } from "@simplewebauthn/browser";
 
 export default class dwarl {
   baseUrl = ''
